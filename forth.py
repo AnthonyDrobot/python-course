d0c4ed20430721d702ab06d75b1b7ac6a08bb972
palladdium <4code4@gmail.com>
2014-04-13 18:31:23 -0400

    forth

--- forth.py
+++ forth.py
@@ -1,20 +1,37 @@
-stack =()
+stack =[]
 
 def eval_forth(file):
-    ch = file.read(1)
     res = ""
-    while ch != '':
-        if ch == '/n':
-            yield res
-            res = ""
-        elif ch == '#':
-            yield res
-            res = ""
+    fl = open(file)
+    ch = fl.readlines()
+    for line in ch:
+        word = str.split(line)
+        print word[0]
+        if word[0] == 'add':
+            if (isinstance(stack[0], int)) is True and (isinstance(stack[1], int)) is True:
+            #if stack[0] == int and stack[1] == int:
+                stack.append(stack.pop(0)+stack.pop(1))
+        elif word[0] == 'put':
+            if (isinstance(word[1], int)) is True:
+            #if word[1] == int:
+                stack.append(word[1])
+            elif (isinstance(word[1], str)) is True:
+            #elif word[1] == str:
+                res = word[1].replace("'", "")
+                res = res.replace('"', '')
+                stack.append(repr(res))
+        elif word[0] == 'pop':
+            stack.pop(0)
+        elif word[0] == 'print':
+            print stack[0]
+        elif word[0] == 'sub':
+            if (isinstance(stack[0], int)) is True and (isinstance(stack[1], int)) is True:
+            #if stack[0] == int and stack[1] == int:
+                stack.append(stack.pop(0)-stack.pop(1))
+        elif word[0][0] == '#':
+            pass
         else:
-            res += ch
-            ch = fd.read(1)
-    yield res
+            raise "Syntax Error - wrong operator", word[0]
 
 
-
-eval_forth('forth.py')
+eval_forth('sample.frt')
